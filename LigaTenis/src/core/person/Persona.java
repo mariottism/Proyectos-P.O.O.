e751d3efda0a18bd0fc1257e42b060dbaad6d369
mariottism <mariottism@uninorte.edu.co>
2026-04-08 17:23:22 -0500

    Persona.java

--- LigaTenis/src/core/person/Persona.java
+++ LigaTenis/src/core/person/Persona.java
@@ -1,1 +1,18 @@
+package core.person;
+/*
 
+
+*/
+public abstract class Persona {
+    
+    protected String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+}
