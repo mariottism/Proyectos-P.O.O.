58016460d676e1216fadac7089b459cd677295a0
mariottism <mariottism@uninorte.edu.co>
2026-04-08 22:54:34 -0500

    Update Package.java

--- CargoLink/src/core/company/package/Package.java
+++ CargoLink/src/core/company/package/Package.java
@@ -1,1 +1,25 @@
+public class Package {
 
+    private float weight;
+    private Content content;
+    private float value;
+
+    public Package(float weight, Content content, float value) {
+        this.weight = weight;
+        this.content = content;
+        this.value = value;
+    }
+
+    // Getters CLAVE (los usa todo el parcial)
+    public float getWeight() {
+        return weight;
+    }
+
+    public Content getContent() {
+        return content;
+    }
+
+    public float getValue() {
+        return value;
+    }
+}
