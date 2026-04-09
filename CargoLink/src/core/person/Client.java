8c6cea6195874a7e9c9f5a89f3e566422d88116b
mariottism <mariottism@uninorte.edu.co>
2026-04-08 21:11:35 -0500

    Update Client.java

--- CargoLink/src/core/person/Client.java
+++ CargoLink/src/core/person/Client.java
@@ -1,1 +1,35 @@
+public class Client {
+  private int id;
+  private String name;
+  private String address;
+  private String email;
+  private ArrayList<Shipping> shippings;
 
+  //Constructor 
+  public Client(int id, String name, String address, String email, Shipping shipping){
+    this.id = id;
+    this.name = name;
+    this.address = address;
+    this.email = email;
+    this.shippings = new ArrayList<>();
+  }
+
+  // Método makeShipping: crear un Shipping y guardarlo en Cliente
+  public void makeShipping(String code, LogisticsCenter originCenter, LogisticsCenter destinationCenter, LocalDate registrationDate, LocalDate deliveryDate, Status status, Vehicle vehicle) {
+    // Cuando el cliente hace un envío, se crea un objeto Shipping y se guarda en su lista
+    // this: el cliente esta creando el envío
+    Shipping shipping = new Shipping(code, this, originCenter, destinationCenter, registrationDate, deliveryDate, status, vehicle);
+    this.shippings.add(shipping); 
+  }
+
+  //Getter busca en la lista shippings con un codigo especifico. Sin recorrer toda la lista retorna al encontrar
+  public Shipping getShipping(String code) {
+    for (Shipping s : this.shippings) {
+        if (s.getCode().equals(code)) {
+            return s;
+        }
+    }
+    return null; // ó System.out.println("Shipping not found");
+}
+
+}
