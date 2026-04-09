65af5d119c90de75aab8da4a5dd4a0829d6f53fd
mariottism <mariottism@uninorte.edu.co>
2026-04-08 22:46:59 -0500

    Update Vehicle.java

--- CargoLink/src/core/company/vehicle/Vehicle.java
+++ CargoLink/src/core/company/vehicle/Vehicle.java
@@ -1,1 +1,25 @@
+import java.util.ArrayList;
 
+public abstract class Vehicle {
+
+    protected float cargoCapacity;
+    protected ArrayList<Shipping> shippings; // Para relación bidireccional
+
+    public Vehicle(double cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+        this.shippings = new ArrayList<>();
+    }
+
+    //  IMPORTANTE: para relación bidireccional
+    public void addShipping(Shipping s) {
+        shippings.add(s);
+    }
+
+    public ArrayList<Shipping> getShippings() {
+        return shippings;
+    }
+
+    public float getCargoCapacity() {
+        return cargoCapacity;
+    }
+}
