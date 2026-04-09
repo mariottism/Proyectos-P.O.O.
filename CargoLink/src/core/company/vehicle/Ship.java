106cd82d10c16428565843011e80dcd4db4fb51f
mariottism <mariottism@uninorte.edu.co>
2026-04-08 22:48:58 -0500

    Update Ship.java

--- CargoLink/src/core/company/vehicle/Ship.java
+++ CargoLink/src/core/company/vehicle/Ship.java
@@ -1,1 +1,13 @@
+public class Ship extends Vehicle {
 
+    private String name;
+
+    public Ship(float cargoCapacity, String name) {
+        super(cargoCapacity);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
