a64fda780dd2fa51f9e037d4738075d875a4fdfc
mariottism <mariottism@uninorte.edu.co>
2026-04-08 22:49:51 -0500

    Update LogisticCenter.java

--- CargoLink/src/core/company/LogisticCenter.java
+++ CargoLink/src/core/company/LogisticCenter.java
@@ -1,1 +1,29 @@
+public class LogisticsCenter {
 
+    private String name;
+    private String city;
+    private String country;
+    private float latitude;
+    private float longitude;
+
+    public LogisticsCenter(String name, String city, String country, float latitude, float longitude) {
+        this.name = name;
+        this.city = city;
+        this.country = country;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    // Getters necesarios para el main
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
