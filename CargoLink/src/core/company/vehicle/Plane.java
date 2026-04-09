66510d7d5e93c8cbeb8bffd90f04b352e41389d1
mariottism <mariottism@uninorte.edu.co>
2026-04-08 22:48:17 -0500

    Update Plane.java

--- CargoLink/src/core/company/vehicle/Plane.java
+++ CargoLink/src/core/company/vehicle/Plane.java
@@ -1,1 +1,13 @@
+public class Plane extends Vehicle {
 
+    private String code;
+
+    public Plane(float cargoCapacity, String code) {
+        super(cargoCapacity);
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
