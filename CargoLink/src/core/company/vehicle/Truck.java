53521efe942ae4e33d44baee15a05cdbd24a1cfb
mariottism <mariottism@uninorte.edu.co>
2026-04-08 22:47:47 -0500

    Update Truck.java

--- CargoLink/src/core/company/vehicle/Truck.java
+++ CargoLink/src/core/company/vehicle/Truck.java
@@ -1,1 +1,13 @@
+public class Truck extends Vehicle {
 
+    private String plate;
+
+    public Truck( float cargoCapacity, String plate) {
+        super(cargoCapacity);
+        this.plate = plate;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+}
